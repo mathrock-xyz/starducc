99898fe4fc3955e80bf8f8879b55f106e4e80d97
simpleajaa <kerjalikeanastronaut@gmail.com>
2025-11-29 03:23:30 +0000

    delete register command and change login.go -> signin.go

--- cmd/register.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/spf13/cobra"
-
-var register = &cobra.Command{
-	Use:   "register",
-	Short: "Create new account",
-	RunE:  func(cmd *cobra.Command, args []string) error {},
-}
